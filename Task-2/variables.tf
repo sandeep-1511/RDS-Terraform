1d4323c7a73cd49e74b30edaea0b6f510d5aaca6
sandeep-1511 <108406587+sandeep-1511@users.noreply.github.com>
2024-05-24 14:21:50 +0530

    Update variables.tf

--- Task-2/variables.tf
+++ Task-2/variables.tf
@@ -1,50 +1,5 @@
 variable "aws_region" {
   description = "The AWS region to deploy the resources."
-  default     = "us-east-1" 
-}
-
-variable "vpc_cidr_block" {
-  description = "The CIDR block for the VPC."
-  default     = "10.0.0.0/16"
-}
-
-variable "public_subnet_cidr_block" {
-  description = "The CIDR block for the public subnet."
-  default     = "10.0.1.0/24"
-}
-
-variable "private_subnet_cidr_block" {
-  description = "The CIDR block for the private subnet."
-  default     = "10.0.2.0/24"
-}
-
-variable "availability_zone" {
-  description = "The availability zone to deploy the resources."
-  default     = "us-east-1a" 
-
-variable "availability_zone2" {
-  description = "The availability zone to deploy the resources."
-  default     = "us-east-1c" 
-}
-
-variable "instance_type" {
-  description = "The EC2 instance type for the auto-scaling group."
-  default     = "t2.micro"
-}
-
-variable "ami_id" {
-  description = "The ID of the Amazon Machine Image (AMI) to use for the instances."
-  default     = "ami-0bb84b8ffd87024d8" 
-}
-
-variable "min_size" {
-  description = "The minimum number of instances in the auto-scaling group."
-  default     = 1
-}
-
-variable "max_size" {
-  description = "The maximum number of instances in the auto-scaling group."
-  default     = 2
 }
 
 variable "db_name" {
@@ -67,8 +22,10 @@ variable "db_instance_class" {
   default     = "db.t3.micro"
 }
 
-variable "security_group_name" {
-  type    = string
-  default = "default_sg"
+variable "security_group" {
+  description = "The security group ID for the RDS instance."
 }
 
+variable "subnet_id" {
+  description = "The subnet ID for the RDS instance."
+}
