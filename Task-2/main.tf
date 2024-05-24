4567b526141f69588b3870d2781061a7db8324d5
sandeep-1511 <108406587+sandeep-1511@users.noreply.github.com>
2024-05-24 14:24:14 +0530

    Update main.tf

--- Task-2/main.tf
+++ Task-2/main.tf
@@ -1,24 +1,110 @@
-resource "aws_db_instance" "rds_instance" {
-  identifier           = "mydb"
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "mysql"
-  engine_version       = "8.0.35"
-  instance_class       = var.db_instance_class
-  username             = var.db_username
-  password             = var.db_password
-  skip_final_snapshot    = true
-  parameter_group_name   = "default.mysql8.0"
-  publicly_accessible  = false
-  vpc_security_group_ids = [var.security_group]
-  db_subnet_group_name   = aws_db_subnet_group.default.name
+provider "aws" {
+  region = var.aws_region
 }
 
-resource "aws_db_subnet_group" "default" {
-  name       = "main"
-  subnet_ids = var.subnet_id
+// Define VPC, subnets, internet gateway, NAT gateway, route tables, and security groups here
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = var.vpc_cidr_block
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = var.public_subnet_cidr_block
+  availability_zone = var.availability_zone
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = var.private_subnet_cidr_block
+  availability_zone = var.availability_zone2
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_subnet.id
+}
+
+resource "aws_eip" "nat" {
+  vpc = true
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
 
   tags = {
-    Name = "My DB subnet group"
+    Name = "public-route-table"
   }
+}
+
+resource "aws_route_table_association" "public_subnet" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_security_group" "ssh_only" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "rds_sg" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]  // Adjust as needed to allow traffic from your VPC
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+module "autoscaling" {
+  source         = "./autoscaling"
+  aws_region     = var.aws_region
+  instance_type  = var.instance_type
+  ami_id         = var.ami_id
+  min_size       = var.min_size
+  max_size       = var.max_size
+  public_subnet = aws_subnet.public_subnet.id
+  security_group = aws_security_group.ssh_only.id
+}
+
+module "rds" {
+  source            = "./rds"
+  aws_region        = var.aws_region
+  db_name           = var.db_name
+  db_username       = var.db_username
+  db_password       = var.db_password
+  db_instance_class = var.db_instance_class
+  security_group    = aws_security_group.rds_sg.id
+  subnet_id         = [aws_subnet.public_subnet.id,aws_subnet.private_subnet.id]
 }
