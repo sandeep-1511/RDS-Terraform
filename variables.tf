2ac21d5ad59b8da0035f44b50d2950f958c54465
sandeep-1511 <108406587+sandeep-1511@users.noreply.github.com>
2024-05-24 11:07:27 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "aws_region" {
   description = "The AWS region to deploy the resources."
-  default     = "us-east-1" // Change this as needed
+  default     = "us-east-1" 
 }
 
 variable "vpc_cidr_block" {
@@ -20,12 +20,12 @@ variable "private_subnet_cidr_block" {
 
 variable "availability_zone" {
   description = "The availability zone to deploy the resources."
-  default     = "us-east-1a" // Change this as needed
+  default     = "us-east-1a" 
 }
 
 variable "availability_zone2" {
   description = "The availability zone to deploy the resources."
-  default     = "us-east-1c" // Change this as needed
+  default     = "us-east-1c" 
 }
 
 variable "instance_type" {
@@ -35,8 +35,7 @@ variable "instance_type" {
 
 variable "ami_id" {
   description = "The ID of the Amazon Machine Image (AMI) to use for the instances."
-  default     = "ami-0bb84b8ffd87024d8" // Replace with your desired AMI ID
-}
+  default     = "ami-0bb84b8ffd87024d8" 
 
 variable "min_size" {
   description = "The minimum number of instances in the auto-scaling group."
@@ -48,4 +47,28 @@ variable "max_size" {
   default     = 2
 }
 
+variable "db_name" {
+  description = "The name of the MySQL database."
+  default     = "mydb"
+}
+
+variable "db_username" {
+  description = "The username for the MySQL database."
+  default     = "admin"
+}
+
+variable "db_password" {
+  description = "The password for the MySQL database."
+  default     = "password123"
+}
+
+variable "db_instance_class" {
+  description = "The DB instance class for the RDS MySQL DB."
+  default     = "db.t3.micro"
+}
+
+variable "security_group_name" {
+  type    = string
+  default = "default_sg"
+}
 
